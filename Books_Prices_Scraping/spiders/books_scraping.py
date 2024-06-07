f2525066b6ffabd001669cb445ed9b79ff1d2acf
Hossam Kamel <hossam180871@bue.edu.eg>
2024-06-07 18:49:21 +0300

    Scraping first trial

--- Books_Prices_Scraping/spiders/books_scraping.py
+++ Books_Prices_Scraping/spiders/books_scraping.py
@@ -8,11 +8,14 @@ class BooksScrapingSpider(CrawlSpider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com"]
 
-    rules = (Rule(LinkExtractor(allow=r"Items/"), callback="parse_item", follow=True),)
-
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths='//li[@class="col-xs-6 col-sm-4 col-md-3 col-lg-3"]/article/h3/a'), callback="parse_item", follow=True,),
+        Rule(LinkExtractor(restrict_xpaths='////li[@class="next"]/a'), ),
+        )
     def parse_item(self, response):
-        item = {}
-        #item["domain_id"] = response.xpath('//input[@id="sid"]/@value').get()
-        #item["name"] = response.xpath('//div[@id="name"]').get()
-        #item["description"] = response.xpath('//div[@id="description"]').get()
-        return item
+        yield {
+            'Title':response.xpath('//div[@class="col-sm-6 product_main"]/h1/text()').get(),
+            'Price':response.xpath('//div[@class="col-sm-6 product_main"]/p[@class="price_color"]/text()').get(),
+            'Availablity':response.xpath('normalize-space((//div[@class="col-sm-6 product_main"]/p[2]/text())[2])').get(),
+            'url':response.url,
+        }
